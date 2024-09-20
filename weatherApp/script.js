d385a84cdfad1b9127a16cf3ee8e79b0a05811b5
kimonie <seunkikiowo@gmail.com>
2024-09-20 11:21:00 +0100

    weather app completed, app now uses dynamic search function

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -1,21 +1,61 @@
-const city = document.querySelector(".city");
+// VARIABLES
+const area = document.querySelector(".city");
 const temp = document.querySelector(".temp");
 const humidity = document.querySelector(".humidity");
 const wind = document.querySelector(".wind");
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+const weather = document.querySelector(".weather");
+const xrror = document.querySelector(".error");
 
+// API DETAILS
 const apiKey = "96f22d712646aa871ea2d5fb99dedc26";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=lagos&units=metric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
 
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+async function checkWeather(city) {
+
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if (response.status == 404) {
+        xrror.style.display = "block";
+        weather.style.display = "none"
+    } else {
+        weather.style.display = "block";
+    }
+
     let data = await response.json();
 
-    city.innerHTML = data.name;
+    // PlACEHOLDER
+    area.innerHTML = data.name;
     temp.innerHTML = Math.round(data.main.temp) + "<sup>o</sup>c</h1>";
     humidity.innerHTML = data.main.humidity + "%";
     wind.innerHTML = data.wind.speed + "km/h";
 
+    if(data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.svg"
+    } 
+    else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.svg"
+    } 
+    else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.svg"
+    } 
+    else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.svg"
+    } 
+    else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.svg"
+    } 
+
+
 }
 
-checkWeather();
+// SEARCH FUNCTION
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
+
+
+checkWeather(city);
