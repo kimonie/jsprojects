18201bf718603e4afd39f24b9f4b538d24fefa12
kimonie <seunkikiowo@gmail.com>
2024-09-22 12:43:15 +0100

    interface completed

---
+++ quizApp/script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            {text: "shark", correct: false}
+            {text: "Blue Whale", correct: true}
+            {text: "Elephant", correct: false}
+            {text: "Giraffe", correct: false}
+        ]
+    }
+]
