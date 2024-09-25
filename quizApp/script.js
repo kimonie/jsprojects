54f45f0ac996b26984f7cd591bc0e8ca7e22827d
kimonie <seunkikiowo@gmail.com>
2024-09-25 22:17:08 +0100

    continued

--- quizApp/script.js
+++ quizApp/script.js
@@ -2,10 +2,49 @@ const questions = [
     {
         question: "Which is the largest animal in the world?",
         answers: [
-            {text: "shark", correct: false}
-            {text: "Blue Whale", correct: true}
-            {text: "Elephant", correct: false}
+            {text: "shark", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Elephant", correct: false},
             {text: "Giraffe", correct: false}
         ]
+    },
+    {
+        question: "Which is the smallest continent?",
+        answers: [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false}
+        ]
     }
-]
+];
+
+
+const questionElement = document.querySelector(".question");
+const answerButton = document.querySelector(".answer-buttons");
+const nextButton = document.querySelector("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    let currenQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currenQuestion.question;
+
+    currenQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn")
+        answerButton.appendChild(button);
+    })
+}
+
+startQuiz();
